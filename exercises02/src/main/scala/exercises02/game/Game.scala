752d14502bf07a64ac78b66fef5980a627113b1a
Nikolay <okaykudes@gmail.com>
2023-03-05 22:05:12 +0500

    Тихончик Николай exercises02

--- exercises02/src/main/scala/exercises02/game/Game.scala
+++ exercises02/src/main/scala/exercises02/game/Game.scala
@@ -12,9 +12,35 @@ class Game(controller: GameController) {
     * Если игрок ввел число больше загаданного, игра должна вызвать controller.numberIsSmaller
     * Если игрок угадал число, игра должна закончиться и вызвать controller.guessed
     * Если игрок написал GameController.IGiveUp, игра должна закончиться и вызвать controller.giveUp(number)
-    * Если игрок ввел неизвестную комбинацию символов, надо вызвать contoller.wrongInput и продолжить игру
+    * Если игрок ввел неизвестную комбинацию символов, надо вызвать controller.wrongInput и продолжить игру
     *
     * @param number загаданное число
     */
-  def play(number: Int): Unit = ???
+  def play(number: Int): Unit = {
+    var guessed = false
+    var input = ""
+    while (!guessed) {
+      controller.askNumber()
+      input = controller.nextLine()
+      try {
+        input.toInt match {
+          case guess if guess < number => controller.numberIsBigger()
+          case guess if guess > number => controller.numberIsSmaller()
+          case guess if guess == number => {
+            controller.guessed()
+            guessed = true
+          }
+        }
+      } catch {
+        case _: NumberFormatException => {
+          if (input == GameController.IGiveUp) {
+            controller.giveUp(number)
+            guessed = true
+          } else {
+            controller.wrongInput()
+          }
+        }
+      }
+    }
+  }
 }
