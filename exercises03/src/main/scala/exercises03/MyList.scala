3d75349ee452b64ed36cb8346c9aee4766d4516e
Nikolay <okaykudes@gmail.com>
2023-03-13 17:10:08 +0500

    Тихончик Николай exercises03

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -8,13 +8,13 @@ case object Nil extends MyList[Nothing]
 
 object MyList {
   def sum(list: MyList[Int]): Int = list match {
-    case Nil => 0
+    case Nil              => 0
     case Cons(head, tail) => head + sum(tail)
   }
 
   def reverse[A](list: MyList[A]): MyList[A] = {
     def helper(list: MyList[A], acc: MyList[A]): MyList[A] = list match {
-      case Nil => acc
+      case Nil              => acc
       case Cons(head, tail) => helper(tail, Cons(head, acc))
     }
 
