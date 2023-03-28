95f09a212a3964b6660b7a41c6971a65363ccdc9
Nikolay <okaykudes@gmail.com>
2023-03-28 17:22:03 +0500

    Тихончик Николай exercises04

--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -7,35 +7,34 @@ class Calculator[T: Integral] {
   def isZero(t: T): Boolean =
     t == implicitly[Integral[T]].zero
 
+  def div(l: Expr[T], r: Expr[T]): Result[T] = {
+    calculate(r) match {
+      case Success(v: T) =>
+        v match {
+          case 0 => DivisionByZero
+          case _ =>
+            calculate(l) match {
+              case Success(w: T) => Success(w / v)
+            }
+        }
+    }
+  }
   def calculate(expr: Expr[T]): Result[T] = expr match {
-    case Mul(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(l), Success(r)) => Success(l * r)
-        case _                        => DivisionByZero
-      }
-    case Div(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (_, Success(r)) if isZero(r) => DivisionByZero
-        case (Success(l), Success(r))     => Success(l / r)
-        case _                            => DivisionByZero
-      }
-    case Plus(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(l), Success(r)) => Success(l + r)
-        case _                        => DivisionByZero
-      }
-    case Minus(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(l), Success(r)) => Success(l - r)
-        case _                        => DivisionByZero
-      }
-    case Val(v) => Success(v)
+    case Mul(left, right)   => applyBinOp(left, right, _ * _)
+    case Div(left, right)   => div(left, right)
+    case Plus(left, right)  => applyBinOp(left, right, _ + _)
+    case Minus(left, right) => applyBinOp(left, right, _ - _)
+    case Val(v)             => Success(v)
     case If(iff, cond, left, right) =>
       calculate(cond) match {
-        case Success(c) =>
-          if (iff(c)) calculate(left)
-          else calculate(right)
-        case _ => DivisionByZero
+        case Success(c) => if (iff(c)) calculate(left) else calculate(right)
+        case _          => DivisionByZero
       }
   }
+
+  private def applyBinOp(left: Expr[T], right: Expr[T], op: (T, T) => T): Result[T] =
+    (calculate(left), calculate(right)) match {
+      case (Success(l), Success(r)) => Success(op(l, r))
+      case _                        => DivisionByZero
+    }
 }
