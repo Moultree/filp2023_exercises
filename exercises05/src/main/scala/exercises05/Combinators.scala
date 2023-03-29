48c1f780e69f9ea806ded1e6a1819fc47978ff51
Nikolay <okaykudes@gmail.com>
2023-03-30 01:08:18 +0500

    Тихончик Ниолай exercises05

--- exercises05/src/main/scala/exercises05/Combinators.scala
+++ exercises05/src/main/scala/exercises05/Combinators.scala
@@ -2,20 +2,6 @@ package exercises05
 import scala.collection.mutable
 
 object Combinators {
-  // Есть цепочка hefEgGeGFEgGgeHE
-  // в данной цепочке есть различные типы частиц
-  // f, e, h, g положительно заряженные частицы
-  // F, E, H, G отрицательно заряженные частицы
-  // если частицы одного типа с разной полярностью стоят вместе в цепочке, они реагируют и исчезают
-  // проход слева направо
-  //
-  // hefEgGeGFEgGgeHE <- gG прореагировали
-  // hefEeGFEgGgeHE <- Ee прореагировали
-  // hefGFEgGgeHE <- gG
-  // hefGFEgeHE <- итоговая цепочка, в которой 10 частиц
-  //
-  // Напишите функцию, используя комбинаторы стандартной библиотеки,
-  // которая проведёт полную реакцию
   def react(ipt: String): String = {
     val q     = mutable.Queue[Char](ipt: _*)
     val stack = mutable.Stack[Char]()
