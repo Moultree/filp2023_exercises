33e8eb4acb9c1ebb95f4cc0e6ce1ddb92d71c9b8
Nikolay <okaykudes@gmail.com>
2023-03-30 01:11:11 +0500

    Тихончик Ниолай exercises05

--- exercises05/src/main/scala/exercises05/Combinators.scala
+++ exercises05/src/main/scala/exercises05/Combinators.scala
@@ -2,18 +2,26 @@ package exercises05
 import scala.collection.mutable
 
 object Combinators {
+  // There is a chain of hefEgGeGFEgGgeHE
+  // Different types of particles are present in this chain:
+  // f, e, h, and g are positively charged particles
+  // F, E, H, and G are negatively charged particles
+  // If particles of the same type but opposite polarity are next to each other in the chain, they react and disappear
+  // The process goes from left to right
+  //
+  // hefEgGeGFEgGgeHE <- gG reacted
+  // hefEeGFEgGgeHE <- Ee reacted
+  // hefGFEgGgeHE <- gG reacted
+  // hefGFEgeHE <- the resulting chain, with 10 particles
+  //
+  // Write a function that uses standard library combinators
+  // to fully react the chain
   def react(ipt: String): String = {
-    val q     = mutable.Queue[Char](ipt: _*)
-    val stack = mutable.Stack[Char]()
-
-    while (q.nonEmpty) {
-      val c = q.dequeue()
-      if (stack.nonEmpty && stack.top != c && stack.top.toLower == c.toLower) {
-        stack.pop()
-      } else {
-        stack.push(c)
-      }
+    val reacted = ipt.foldLeft(List[Char]()) {
+      case (head :: tail, c) if head != c && head.toLower == c.toLower => tail
+      case (acc, c)                                                    => c :: acc
     }
-    stack.reverse.mkString
+    if (reacted.length == ipt.length) reacted.reverse.mkString
+    else react(reacted.reverse.mkString)
   }
 }
