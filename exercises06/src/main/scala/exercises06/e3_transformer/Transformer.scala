a5321f3b95d9d2c68b3b35c2f641f09caa48dc63
Nikolay <okaykudes@gmail.com>
2023-04-05 20:51:38 +0500

    Тихончик Николай exercises06

--- exercises06/src/main/scala/exercises06/e3_transformer/Transformer.scala
+++ exercises06/src/main/scala/exercises06/e3_transformer/Transformer.scala
@@ -9,10 +9,38 @@ trait Transformer[A, B] {
 }
 
 object TransformerInstances {
-  implicit val transformer: Transformer[RawUser, User] = ???
+  implicit val transformer: Transformer[RawUser, User] = new Transformer[RawUser, User] {
+    override def toOption(a: RawUser): Option[User] = toEither(a).toOption
+
+    override def toEither(a: RawUser): Either[Error, User] = {
+      val id =
+        try {
+          a.id.toLong
+        } catch {
+          case _: NumberFormatException => return Left(InvalidId)
+        }
+
+      def nonEmptyString(s: Option[String], err: Error): Either[Error, String] =
+        s.filter(_.nonEmpty).toRight(err)
+
+      for {
+        firstName  <- nonEmptyString(a.firstName, InvalidName)
+        secondName <- nonEmptyString(a.secondName, InvalidName)
+        thirdName  <- Right(a.thirdName.filter(_.nonEmpty))
+      } yield User(id, UserName(firstName, secondName, thirdName))
+    }
+  }
 }
 
-object TransformerSyntax {}
+object TransformerSyntax {
+  implicit class TransformerOps[A](a: A) {
+    def transformToOption[B](implicit transformer: Transformer[A, B]): Option[B] =
+      transformer.toOption(a)
+
+    def transformToEither[B](implicit transformer: Transformer[A, B]): Either[Error, B] =
+      transformer.toEither(a)
+  }
+}
 
 object Examples {
   import TransformerInstances._
--- exercises06/src/main/scala/exercises06/e4_eq/Eq.scala
+++ exercises06/src/main/scala/exercises06/e4_eq/Eq.scala
@@ -6,9 +6,52 @@ trait Eq[A] {
 
 object Eq {}
 
-object EqInstances {}
+object EqInstances {
+  implicit val intEq: Eq[Int] = new Eq[Int] {
+    def eqv(a: Int, b: Int): Boolean = a == b
+  }
 
-object EqSyntax {}
+  implicit val boolEq: Eq[Boolean] = new Eq[Boolean] {
+    def eqv(a: Boolean, b: Boolean): Boolean = a == b
+  }
+
+  implicit def eqOption[A](implicit eqInst: Eq[A]): Eq[Option[A]] = new Eq[Option[A]] {
+    override def eqv(a: Option[A], b: Option[A]): Boolean =
+      (a, b) match {
+        case (Some(aVal), Some(bVal)) => eqInst.eqv(aVal, bVal)
+        case (None, None)             => true
+        case _                        => false
+      }
+  }
+
+  implicit def eqList[A](implicit eqInst: Eq[A]): Eq[List[A]] = new Eq[List[A]] {
+    override def eqv(a: List[A], b: List[A]): Boolean = {
+      val aLen = a.length
+      val bLen = b.length
+      if (aLen == bLen) {
+        val sameLen = a.zip(b).count(x => eqInst.eqv(x._1, x._2))
+        if (sameLen == aLen) true else false
+      } else false
+    }
+  }
+
+  implicit val listIntEq: Eq[List[Int]] = new Eq[List[Int]] {
+    def eqv(a: List[Int], b: List[Int]): Boolean = a.equals(b)
+  }
+
+  implicit val listBoolEq: Eq[List[Boolean]] = new Eq[List[Boolean]] {
+    def eqv(a: List[Boolean], b: List[Boolean]): Boolean = a == b
+  }
+
+}
+
+object EqSyntax {
+  implicit class EqOps[A](val a: A) {
+    def eqv(b: A)(implicit ev: Eq[A]): Boolean = ev.eqv(a, b)
+    def ===(b: A)(implicit ev: Eq[A]): Boolean = eqv(b)
+    def !==(b: A)(implicit ev: Eq[A]): Boolean = !eqv(b)
+  }
+}
 
 object Examples {
   import EqInstances._
@@ -17,7 +60,7 @@ object Examples {
   1 eqv 1 // возвращает true
   1 === 2 // возвращает false
   1 !== 2 // возвращает true
-  // 1 === "some-string" // не компилируется
+  //1 === "some-string" // не компилируется
   // 1 !== Some(2) // не компилируется
   List(true) === List(true) // возвращает true
 }
